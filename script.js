194ea6b57b35a532c250b2146f0d0353fca817a5
Peace Daniel <peacedaniel612@gmail.com>
2024-04-17 17:46:55 +0100

    Updated assignment based on what was tasked

---
+++ script.js
@@ -0,0 +1,39 @@
+function total() {
+    const first = parseFloat(document.getElementById("firstNumber").value);
+    const second = parseFloat(document.getElementById("secondNumber").value);
+    let totalNum = first + second;
+    alert(totalNum);
+    console.log(totalNum);
+}
+
+function difference() {
+    const first = parseFloat(document.getElementById("firstNumber").value);
+    const second = parseFloat(document.getElementById("secondNumber").value);
+    let differenceNum = first - second;
+    alert(differenceNum);
+    console.log(differenceNum);
+}
+
+function multiply() {
+    const first = parseFloat(document.getElementById("firstNumber").value);
+    const second = parseFloat(document.getElementById("secondNumber").value);
+    let multiplyNum = first * second;
+    alert(multiplyNum);
+    console.log(multiplyNum);
+}
+
+function divide() {
+    const first = parseFloat(document.getElementById("firstNumber").value);
+    const second = parseFloat(document.getElementById("secondNumber").value);
+    let divideNum = first / second;
+    alert(divideNum);
+    console.log(divideNum);
+}
+
+function modulus() {
+    const first = parseFloat(document.getElementById("firstNumber").value);
+    const second = parseFloat(document.getElementById("secondNumber").value);
+    let modulusNum = first % second;
+    alert(modulusNum);
+    console.log(modulusNum);
+}
