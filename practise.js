179d77f4275b9a9456fcada2caddaf401fa5dfad
Peace Daniel <peacedaniel612@gmail.com>
2024-04-18 17:25:49 +0100

    installing node

---
+++ practise.js
@@ -0,0 +1,12 @@
+//Conditionals//
+var grades = 9;
+var absence = 2;
+
+if (grades >= 7 && absence <= 5) {
+    console.log('Student is approved');
+} 
+else{
+    console.log('student not approved');
+}
+
+console.log('this is a test');
